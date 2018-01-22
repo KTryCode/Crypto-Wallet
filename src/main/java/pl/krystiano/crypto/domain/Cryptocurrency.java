42cda669be3a65c6868ccdb9841f48833289e8c2
krystianl <krystian.leszczewski@nask.pl>
2018-01-22 13:06:34 +0100

    Small changes of wallet displaying

--- src/main/java/pl/krystiano/crypto/domain/Cryptocurrency.java
+++ src/main/java/pl/krystiano/crypto/domain/Cryptocurrency.java
@@ -3,21 +3,18 @@ package pl.krystiano.crypto.domain;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
 @Data
-@Getter
-@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Cryptocurrency {
 
-    public Cryptocurrency() {
-    }
 
     public Cryptocurrency(String name, String shortname, double amount, LocalDate dateOfPurchase, double courseOnPurchaseDate) {
         this.name = name;
@@ -30,17 +27,12 @@ public class Cryptocurrency {
     @Id
     @GeneratedValue
     private long id;
-    @NotNull
     private String name;
-    @NotNull
     private String shortname;
-    @NotNull
     private double amount;
-    @NotNull
+    private double courseOnPurchaseDate;
     @JsonFormat(pattern = "MM/dd/yyyy")
     private LocalDate dateOfPurchase;
-    @NotNull
-    private double courseOnPurchaseDate;
 
 }
 
