faec8ddc627e416a5d7119d07d88224887083f8d
krystianl <krystian.leszczewski@nask.pl>
2018-02-05 15:14:08 +0100

    Revert "CoinData model added"
    This reverts commit 49e84f5

--- src/main/webapp/src/app/crypto/coin.model.ts
+++ src/main/webapp/src/app/crypto/coin.model.ts
@@ -1,19 +1,14 @@
-import {CoinData} from "./coin_data.model";
-
 export class Coin{
   public symbol: string;
   public amount: number;
   public price_usd: number;
   public value: number;
-  public coin_data: CoinData;
 
 
-  constructor(symbol: string, amount: number, price_usd: number, value: number, coin_data: CoinData) {
+  constructor(symbol: string, amount: number, price_usd: number, value: number) {
     this.symbol = symbol;
     this.amount = amount;
     this.price_usd=price_usd;
     this.value=value;
-    this.coin_data=coin_data;
   }
-
 }
